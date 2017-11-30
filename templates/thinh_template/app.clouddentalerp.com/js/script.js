fa1db7d0c09670b56fd6dd7774f99f3e2df8632c
hoangducthinh2012 <hoangducthinh2012@gmail.com>
2017-11-30 11:07:06 +0700

    Nov 30 update

--- templates/thinh_template/app.clouddentalerp.com/js/script.js
+++
@@ -1,8 +0,0 @@
-// Tooltip
-    $(document).ready(function(){
-        $('[data-toggle="tooltip"]').tooltip();
-    });
-// Popover
-    $(document).ready(function(){
-        $('[data-toggle="popover"]').popover();
-    });
